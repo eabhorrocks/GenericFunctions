3883f28931626ebeaab80abfc61bcf8feb1f6e5a
eabhorrocks <36156626+eabhorrocks@users.noreply.github.com>
2023-10-19 12:53:42 +0100

    Update convert3D_to_angV.m

--- convert3D_to_angV.m
+++ convert3D_to_angV.m
@@ -3,11 +3,11 @@ function w_deg = convert3D_to_angV(pos0_x, pos0_z, eyePos_x, eyePos_z, vel_x, ve
 % Based on Rokers et al., 2018 derivations.
 
 %% example inputs. Unit is arbitrary but must be consistent.
-% pos0_x = 25;
-% pos0_z = 25;
+% pos0_x = 100;
+% pos0_z = 100;
 % 
 % vel_x = 0;
-% vel_z = 15;
+% vel_z = 135;
 % 
 % eyePos_x = 0;
 % eyePos_z = 0;
@@ -17,6 +17,7 @@ function w_deg = convert3D_to_angV(pos0_x, pos0_z, eyePos_x, eyePos_z, vel_x, ve
 %B = atan((pos0_z/(pos0_x-eyePos_z)));
 
 %% main part of function
+
 % get initial distance of object to eye
 h0 = sqrt((pos0_x-eyePos_x)^2 + (pos0_z-eyePos_z)^2);
 
